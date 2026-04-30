4d731e129d5bf249995c88051a89e0f63d7d9409
chkecom1905-code <chkecom1905@gmail.com>
2026-04-30 17:04:06 +0200

    Update supabase_config.dart

--- athlytix/lib/supabase_config.dart
+++ athlytix/lib/supabase_config.dart
@@ -1,4 +1,5 @@
 // lib/supabase_config.dart
 
-const String supabaseUrl = 'YOUR_SUPABASE_URL';
-const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+// lib/supabase_config.dart
+const String supabaseUrl = 'https://rohxbqqetzqdaiabenji.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvaHhicXFldHpxZGFpYWJlam5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NTQ1MzYsImV4cCI6MjA5MzEzMDUzNn0.SOrlsabQfKqTJwynU0SkX0GSouy6ailAGje3jY5yrhw';
